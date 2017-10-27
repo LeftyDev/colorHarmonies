e293cc731b23fa1a627d9a0e8e2c111f0a1ab31f
Lefty <brjebrow@umail.iu.edu>
2017-10-27 01:10:53 -0400

    Final Commit of ConceptualCafe

--- n320projects/ConceptualCafe/js/app.js
+++ n320projects/ConceptualCafe/js/app.js
@@ -6,11 +6,10 @@ Vue.component("customer", {
     template: "#customerTemplate",
     //initiate return data
     data: function () {
-        return {
-        }
+        return {}
     },
     methods: {
-        choice: function(customerChoice) {
+        choice: function (customerChoice) {
             this.customerChoice = customerChoice;
 
             if (this.customerChoice == "dripBrew") {
@@ -35,59 +34,174 @@ Vue.component("customer", {
 
 var barista = Vue.component("barista", {
     template: "#baristaTemplate",
-    data: function() {
+    data: function () {
         return {
+            baristaPrompt: "Hello, how may I help you today?",
             customerChoice: "",
-            baristaPrompt: "How may I help you today?"
+            choiceValue: "",
+            machineMake: "",
+            finishedBrew: ""
         }
     },
-    props: [ "choice" ],
+    //props are passed down from parent, in this case choice from App's this.choice
+    //initiate prop names to be used by <barista> tag for v-bind statements
+    props: [
+        "choice",
+        "choicevalue",
+        "machinemake",
+        "finished"
+    ],
     watch: {
-        choice: function(choice) {
+        choice: function (choice) {
             this.choice = choice;
 
-            if (this.choice == "App now knows you ordered drip.") {
-                this.customerChoice = "Barista now knows you ordered drip."
+            if (this.choice == "drip brew") {
+                this.customerChoice = "One " + this.choice + "?";
+                this.$emit('machinedrip');
             }
-            if (this.choice == "App now knows you ordered french press.") {
-                this.customerChoice = "Barista now knows you ordered french press."
+            if (this.choice == "french press") {
+                this.customerChoice = "One " + this.choice + "?";
+                this.$emit('machinefrench');
             }
-            if (this.choice == "App now knows you ordered aeropress.") {
-                this.customerChoice = "Barista now knows you ordered aeropress."
+            if (this.choice == "aeropress") {
+                this.customerChoice = "One " + this.choice + "?";
+                this.$emit('machineaero');
             }
+
+            this.baristaPrompt = "";
+            this.machineMake = "";
+            this.finishedBrew = "";
+        },
+        choicevalue: function (choiceValue) {
+            this.choiceValue = "That will be $" + choiceValue + ".";
+        },
+        machinemake: function (machineMake) {
+            this.machineMake = "Waiting for " + machineMake;
+            this.customerChoice = "";
+            this.choiceValue = "";
+        },
+        finished: function (finished) {
+            this.machineMake = "";
+            this.finishedBrew = "Your " + finished + ". Here you go!";
         }
     }
 });
 
-Vue.component("coffeeMachine", {
-    template: "#machineTemplate"
+Vue.component("coffee-machine", {
+    template: "#machineTemplate",
+    data: function () {
+        return {
+            machineMake: "nothing",
+            brewing: ""
+        }
+    },
+    props: ["machinemake", "brewing"],
+    watch: {
+        machinemake: function (machineMake) {
+            this.machineMake = machineMake;
+
+        },
+        brewing: function (makingCoffee) {
+            this.brewing = makingCoffee;
+            if (this.brewing == "brewing drip...") {
+                setTimeout(() => {
+                    console.log("Your drip brew is done");
+                    this.$emit("dripbrew-finished");
+                    this.machineMake = "nothing";
+                }, 10000);
+            }
+
+            if (this.brewing == "brewing french...") {
+                setTimeout(() => {
+                    console.log("Your french press is done");
+                    this.$emit("frenchpress-finished");
+                    this.machineMake = "nothing";
+                }, 10000);
+            }
+
+            if (this.brewing == "brewing aero...") {
+                setTimeout(() => {
+                    console.log("Your aeropress is done");
+                    this.$emit("aeropress-finished");
+                    this.machineMake = "nothing";
+                }, 10000);
+            }
+        }
+    }
+
 });
 
 
 new Vue({
     el: "#app",
-    data: function() {
-        alert("App is working.");
+    data: function () {
+        console.log("App is working.");
+
         //initiate return data
         return {
-            choice: "nothing"
+            choice: "nothing",
+            choiceValue: "0.00",
+            machineMake: "nothing",
+            makingCoffee: "",
+            finished: "nothing"
         }
     },
     methods: {
+
+        //customer on events
+
         makeDrip: function () {
-            this.choice = "App now knows you ordered drip.";
-            alert(this.choice);
-            //now we need to send this data to the barista
+            this.choice = "drip brew";
+            this.choiceValue = "3.00";
+            //now we need to grab this data in the barista using v-bind
         },
         makeFrench: function () {
-            this.choice = "App now knows you ordered french press.";
-            alert(this.choice);
-            //now we need to send this data to the barista
+            this.choice = "french press";
+            this.choiceValue = "4.00";
+            //now we need to grab this data in the barista using v-bind
         },
         makeAero: function () {
-            this.choice = "App now knows you ordered aeropress.";
-            alert(this.choice);
-            //now we need to send this data to the barista
+            this.choice = "aeropress";
+            this.choiceValue = "5.00";
+            //now we need to grab this data in the barista using v-bind
+        },
+
+        //barista on events
+
+        machineDrip: function () {
+            setTimeout(() => {
+                this.machineMake = "drip brew";
+                console.log("Currently brewing coffee for the next ten seconds...");
+                this.makingCoffee = "brewing drip...";
+            }, 1500);
+
+        },
+        machineFrench: function () {
+            setTimeout(() => {
+                this.machineMake = "french press";
+                console.log("Currently brewing coffee for the next ten seconds...");
+                this.makingCoffee = "brewing french...";
+            }, 1500);
+
+        },
+        machineAero: function () {
+            setTimeout(() => {
+                this.machineMake = "aeropress";
+                console.log("Currently brewing coffee for the next ten seconds...");
+                this.makingCoffee = "brewing aero...";
+            }, 1500);
+        },
+
+        //coffee-machine on events
+
+        dripFinished: function() {
+            this.finished = "drip brew is finished";
+        },
+        frenchFinished: function() {
+            this.finished = "french press is finished";
+        },
+        aeroFinished: function() {
+            this.finished = "aeropress is finished";
         }
     }
 });
